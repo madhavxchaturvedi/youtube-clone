828663e0ee627593174877e100309b3e1f3e5350
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2024-11-15 18:26:34 +0530

    add .env file

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,4 +1,4 @@
-export const AUTH_KEY = "AIzaSyCouIqA2KSp3pcJW1Syn4HcSqXysJi235g";
+export const AUTH_KEY = process.env.REACT_APP_AUTH_KEY;
 
 export const LIVE_CHAT_COUNT = 20;
 
@@ -6,7 +6,7 @@ export const YOUTUBE_VIDEO_API =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   AUTH_KEY;
 
-  export const YOUTUBE_VIDEO_BYID = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${AUTH_KEY}&id=`;
+export const YOUTUBE_VIDEO_BYID = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${AUTH_KEY}&id=`;
 
 export const YOUTUBE_SEARCH_API =
   "https://youtube-search-suggestion.onrender.com/?q=";
@@ -17,12 +17,12 @@ export const YOUTUBE_SEARCH_RESULTS_API =
 export const YOUTUBE_CHANNEL_INFO_API =
   "https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=";
 
-  export function numFormatter(num) {
-    if (num > 999 && num < 1000000) {
-      return (num / 1000).toFixed(1) + "K"; // convert to K for number from > 1000 < 1 million
-    } else if (num > 1000000) {
-      return (num / 1000000).toFixed(1) + "M"; // convert to M for number from > 1 million
-    } else if (num < 900) {
-      return num; // if value < 1000, nothing to do
-    }
+export function numFormatter(num) {
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(1) + "K"; // convert to K for number from > 1000 < 1 million
+  } else if (num > 1000000) {
+    return (num / 1000000).toFixed(1) + "M"; // convert to M for number from > 1 million
+  } else if (num < 900) {
+    return num; // if value < 1000, nothing to do
   }
+}
